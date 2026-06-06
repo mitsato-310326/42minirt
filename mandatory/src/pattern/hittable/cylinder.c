3e498ba6bd90015115fc85c7e0c736c32b3b550e
mitsato-310326☆ <satou310renrakuyou@gmail.com>
2026-06-06 20:20:43 +0900

    fix: set_face_normalは一時的に対比させた

--- mandatory/src/pattern/hittable/cylinder.c
+++ mandatory/src/pattern/hittable/cylinder.c
@@ -6,20 +6,20 @@
 /*   By: mitsato <mitsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 16:37:15 by mitsato           #+#    #+#             */
-/*   Updated: 2026/06/06 19:41:45 by mitsato          ###   ########.fr       */
+/*   Updated: 2026/06/06 20:20:00 by mitsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static void set_face_normal(t_ray *r, t_vec_three *outward_normal, t_hit_record *rec)
-{
-	rec->front_face = dot(r->v_dir, *outward_normal) < 0;
-	if (rec->front_face)
-		rec->normal = *outward_normal;
-	else
-		rec->normal = vec_three_mult(*outward_normal, -1.0);
-}
+// static void set_face_normal(t_ray *r, t_vec_three *outward_normal, t_hit_record *rec)
+// {
+// 	rec->front_face = dot(r->v_dir, *outward_normal) < 0;
+// 	if (rec->front_face)
+// 		rec->normal = *outward_normal;
+// 	else
+// 		rec->normal = vec_three_mult(*outward_normal, -1.0);
+// }
 
 bool hit_cylinder(double t_min, double t_max, void *cylinder, t_ray *r, t_hit_record *rec)
 {
@@ -42,8 +42,8 @@ bool hit_cylinder(double t_min, double t_max, void *cylinder, t_ray *r, t_hit_re
 		if (temp < t_max && temp > t_min) {
 			rec->t = temp;
 			rec->p = ray_at(*r, rec->t);
-			t_vec_three outward_normal = vec_three_mult(vec_three_neg(rec->p, *center), 1 / radius);
-			set_face_normal(r, &outward_normal, rec);
+			// t_vec_three outward_normal = vec_three_mult(vec_three_neg(rec->p, *center), 1 / radius);
+			// set_face_normal(r, &outward_normal, rec);
 			rec->material = ((t_hittable *)cylinder)->material;
 			return true;
 		}
@@ -51,8 +51,8 @@ bool hit_cylinder(double t_min, double t_max, void *cylinder, t_ray *r, t_hit_re
 		if (temp < t_max && temp > t_min) {
 			rec->t = temp;
 			rec->p = ray_at(*r, rec->t);
-			t_vec_three outward_normal = vec_three_mult(vec_three_neg(rec->p, *center), 1 / radius);
-			set_face_normal(r, &outward_normal, rec);
+			// t_vec_three outward_normal = vec_three_mult(vec_three_neg(rec->p, *center), 1 / radius);
+			// set_face_normal(r, &outward_normal, rec);
 			rec->material = ((t_hittable *)cylinder)->material;
 			return true;
 		}
