bc8f50dbe08ab572d497d0bb0675a12ca8a01d9c
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2026-08-12 18:36:15 +0900

    restore: lines.c

---
+++ mandatory/src/parser/lines.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lines.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keitotak <keitotak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/25 14:45:58 by keitotak          #+#    #+#             */
+/*   Updated: 2026/08/12 18:35:30 by keitotak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+bool	empty_line(char *str)
+{
+	while (*str)
+		if (!ft_isspace(*str++))
+			return (false);
+	return (true);
+}
+
+static void	remove_empty_line(t_list **lines)
+{
+	t_list	*node;
+
+	while (*lines)
+	{
+		if (empty_line((*lines)->content))
+		{
+			node = *lines;
+			*lines = node->next;
+			free(node->content);
+			free(node);
+		}
+		else
+			lines = &(*lines)->next;
+	}
+}
+
+t_list	*get_list(char *str, char const *set)
+{
+	char	**arr;
+	t_list	*lines;
+
+	arr = my_split(str, set);
+	if (arr == NULL)
+		return (put_error("split", 1), NULL);
+	lines = arr_to_lst(arr);
+	free_array(arr, arrlen(arr));
+	remove_empty_line(&lines);
+	return (lines);
+}
